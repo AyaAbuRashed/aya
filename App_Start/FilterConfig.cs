1f5606124a1205d53fe2702c31f7af4709c4b9e6
AyaAbuRashed <70090852+AyaAbuRashed@users.noreply.github.com>
2021-01-02 04:16:04 +0200

    Add files via upload

--- App_Start/FilterConfig.cs
+++ App_Start/FilterConfig.cs
@@ -1,7 +1,7 @@
 ﻿using System.Web;
 using System.Web.Mvc;
 
-namespace InventorySystem
+namespace InventoryTaskWebApi
 {
     public class FilterConfig
     {
--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace InventorySystem
+namespace InventoryTaskWebApi
 {
     public class RouteConfig
     {
